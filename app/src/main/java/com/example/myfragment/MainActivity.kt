1c05cabf9d46776f6e0f0613b053814f42eb771d
Gayane Aleksanyan <galexanyan@gmail.com>
2023-05-25 02:45:42 +0400

    implementing transaction between fragments

--- app/src/main/java/com/example/myfragment/MainActivity.kt
+++ app/src/main/java/com/example/myfragment/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.myfragment
 
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, FirstFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun startSecondFragment(imageButton: ImageView) {
+        supportFragmentManager
+            .beginTransaction()
+            .addSharedElement(imageButton, "transition_1")
+            .addToBackStack(null)
+            .replace(R.id.fragment_container, SecondFragment())
+            .commit()
+    }
+
+    fun startExampleFragment(textView: TextView) {
+        supportFragmentManager
+            .beginTransaction()
+            .addSharedElement(textView, "tv_android")
+            .addToBackStack(null)
+            .replace(R.id.fragment_container, ExampleFragment())
+            .commit()
     }
 }
--- app/src/main/java/com/example/myfragment/SecondFragment.kt
+++ app/src/main/java/com/example/myfragment/SecondFragment.kt
@@ -2,12 +2,13 @@ package com.example.myfragment
 
 import android.os.Bundle
 import android.transition.TransitionInflater
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_second.tv_android
 
-class ExampleFragment : Fragment() {
+class SecondFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +21,14 @@ class ExampleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_example, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tv_android.setOnClickListener {
+            (activity as MainActivity).startExampleFragment(tv_android)
+        }
     }
 }
